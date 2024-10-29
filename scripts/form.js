e4f304171ea1dcaf6df22abdcf56d26038a2b148
Tayoanalist <martinstayo20@gmail.com>
2024-10-29 02:55:08 +0100

    form

--- scripts/form.js
+++ scripts/form.js
@@ -13,6 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
     productSelect.appendChild(option);
   });
 
-  const currentYear = new Date().getFullYear();
-  document.getElementById('currentYear').textContent = currentYear;
+  // Set the current year
+const yearSpan = document.getElementById('currentyear');
+yearSpan.textContent = new Date().getFullYear();
+
+// Set last modified date
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = `Last modified: ${document.lastModified}`;
 });
